4b10d50b51e6b8ad6179ad56d7e1aaab5d29e5c3
ahmedeidoox <ahmedeidox99@gmail.com>
2024-06-02 22:39:45 +0200

    project structure

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+    "name": "MedixCare Clinic Management System",
+    "author":"Ahmed Eid Abdallah",
+    "category":"",
+    "version":"17.0.0.1.0",
+    "depends":["base"],
+    "data":[
+        "security/security.xml",
+        "security/ir.model.access.csv",
+        "views/base_menu.xml",
+        "views/patients_view.xml",
+            ],
+    "application":True,
+
+
+}
